5c8763d35694dfc77c60ba2be6596d846485fdbe
M Hamid Jamil <55445517+MHamidJamil420@users.noreply.github.com>
2021-12-20 22:45:06 +0500

    delay_finalized_with_readystate

--- Files/Face_identification.py
+++ Files/Face_identification.py
@@ -2,6 +2,8 @@ import cv2
 import serial
 import time
 arduino = serial.Serial(port='COM11', baudrate=9600, timeout=.1)
+
+
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
@@ -9,13 +11,19 @@ def write_read(x):
     return data
 
 time.sleep(3)
+print(write_read("26"))
+time.sleep(2)
+# 26 to use sensor 1
+# 980 to use sensor 2
+
+# time.sleep(3)
 # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 # net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 video = cv2.VideoCapture(0)
 # load "haarcascade_frontalface_default.xml" by creating a CascadeClassifier
 # object as cascade
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
+imgDetected = False
 while True:
     check, frame = video.read()
 
@@ -33,8 +41,14 @@ while True:
     # using for loop to go through the locations x,y,w,h and drow a rectangle
     for x, y, w, h in face:
         frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 3)
-        print(write_read("98"))
-    print(write_read("98"))
+    #     if not imgDetected:
+    #         print(write_read("98"))
+    #         imgDetected = True
+    #         time.sleep(1)
+    # if imgDetected:
+    #     print(write_read("44"))
+    #     time.sleep(1)
+    #     imgDetected = False
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
     if(key == ord('q')):
